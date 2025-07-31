523d75daf7c24e819b31a3cafbc98ef89f3dc62f
DESKTOP-NOP5UKF\jaege <jaeger6060@gmail.com>
2025-07-31 22:53:00 +0200

    made api to accept requests from host specified in application.properties

--- src/main/java/com/baljeet/api/Chess/ChessController.java
+++ src/main/java/com/baljeet/api/Chess/ChessController.java
@@ -2,6 +2,7 @@ package com.baljeet.api.Chess;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/projects/chess")
@@ -9,20 +10,21 @@ public class ChessController {
     Game game;
 
     @PutMapping("/start")
-    public void startGame(ChessRequests.StartGameRequest request){
+    public ResponseEntity<Map<String,String>> startGame(@RequestBody ChessRequests.StartGameRequest request){
         String fen = request.fen;
         boolean engine = request.engine;
         game = new Game(fen,engine);
+		return ResponseEntity.ok(Map.of("Status","Game Started successfully!"));
 
     }
     @GetMapping("/getMoves")
-    public ResponseEntity<ChessResponses.getMovesResponse> getMoves(ChessRequests.getAllMovesRequest request){
+    public ResponseEntity<ChessResponses.getMovesResponse> getMoves(@RequestBody ChessRequests.getAllMovesRequest request){
         if(game == null) return ResponseEntity.badRequest().build();
         return ResponseEntity.ok(game.getMoves(request.square));
     }
 
     @PostMapping("/makeMove")
-    public ResponseEntity<ChessResponses.makeMoveResponse> makeMove(ChessRequests.makeMove request){
+    public ResponseEntity<ChessResponses.makeMoveResponse> makeMove(@RequestBody ChessRequests.makeMove request){
         if(game == null) return ResponseEntity.badRequest().build();
         return ResponseEntity.ok(game.makeMove(request.move));
     }
